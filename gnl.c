7a58428c5ba59f93c19a7ba19934ad82d9065411
kobe <thodavid@student.42.fr>
2025-07-03 13:53:01 +0200

    Update gnl.c

--- gnl.c
+++ gnl.c
@@ -28,6 +28,36 @@ char    *ft_strdup(char *src)
     return (new);
 }
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 
+#endif
+
+
+char    *ft_strdup(char *src)
+{
+    int        i;
+    char    *new;
+
+    i = 0;
+    while (src[i])
+        i++;
+    new = malloc(sizeof(char)* (i + 1));
+    i = 0;
+    while (src[i])
+    {
+        new[i] = src[i];
+        i++;
+    }
+    new[i] = '\0';
+    return (new);
+}
+
 char    *get_next_line(int fd)
 {
     static char        buffer[BUFFER_SIZE];
@@ -56,6 +86,26 @@ char    *get_next_line(int fd)
     if (i == 0)
         return (NULL);
     return (ft_strdup(line));
+}
+
+int	main()
+{
+	int	fd;
+	char	*line;
+
+	fd = open("text.txt", O_RDONLY);
+	if (fd < 0)
+		return (1);
+printf("\n==fd is %i==\n",fd);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break;
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
 }
 
 int	main()
